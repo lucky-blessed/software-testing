3abecbe79884049b5f442d757b10a15ef042ba9d
Lucky-Blessed <blessednkwor7@gmail.com>
2026-02-11 18:58:56 -0700

    update

--- learning_pytest/todo_list/test_todo_list.py
+++ learning_pytest/todo_list/test_todo_list.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 import time 
 import http.server
 import socketserver
@@ -15,6 +15,9 @@ def start_server(port=8000):
     # Start a HTTP server in a backgroung thread
 
     global server, server_thread
+    # Change the working directory of the HTML file
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
 
     # Create a HTTP sever 
     handler = http.server.SimpleHTTPRequestHandler
@@ -27,7 +30,7 @@ def start_server(port=8000):
     print(f'Server is connected at http://localhost:{port}')
     time.sleep(1) # Gives the server a moment to start
 
-
+# Stop server
 def stop_server():
     global server
     if server:
@@ -35,22 +38,32 @@ def stop_server():
         print('Server is shutdown')
 
 
-def test_add_todo_item():
-    print("\n===Test: Add To-Do Item===")
+def test_add_todo_item(page: Page):
+    print('\n=== Runing test: Add To-Do Item ===')
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, slow_mo=500)
-        page = browser.new_page()
+    # Navigate to the app
+    page.goto('http://localhost:8000/todo_list.html')
+    print('Adding a new task...')
 
-        # Open the app
-        page.goto("http://localhost:8000")
+    # Find the input element and fill it in with new task
+    page.get_by_test_id('todo-input').fill('Buy groceries')
 
-        # Type in the task
-        print("Adding new task...")
-        page.get_by_test_id('todo-input').fill("Buy groceries")
+    # Click the submit butten
+    page.get_by_role("button", name="Submit").click()
 
-        time.sleep(1)
-        browser.close()
+    page.wait_for_timeout(500)
+
+    # Verify that the tast appears on the UI list
+    todo_items = page.get_by_test_id('todo-item')
+    assert todo_items.count() == 1, "Should have 1 to-do item after adding"
+
+    # Verify the counter update
+    total_count = page.locator('#total-count').text_content()
+    assert total_count == 1, f'Total count should be 1 after adding, but got {total_count}'
+
+    print("Task added succefully!")
+
+    page.wait_for_timeout(500)
 
 # Run out suite of test
 def run_all_tests():
